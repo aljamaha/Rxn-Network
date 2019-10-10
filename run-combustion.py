c73f506b19eddbebcb42262726e6f1b7bf0501ad
aljamaha <aljamaha@stanford.edu>
2019-10-10 11:55:57 -0700

    run-comubstion print fns are updated

--- run-combustion.py
+++ run-combustion.py
@@ -4,13 +4,12 @@ import sys
 
 reactant = sys.argv[1]	#name of the hydrocarbon reactant
 
-print 'generate species ..'
-os.system('python generate-species.py '+reactant) #dict of is/ts/fs
-#print '='*20, 'reduce species'
-#os.system('python reduce-species.py '+reactant) #eliminate redundant speciesa
-#print 'does this also include removing O in adjacent carbons?'
-#print '='*20, 'catmap'
-#os.system('python catmap-26.py '+reactant)
+print 'generating reactions list ...'
+os.system('python generate-species.py '+reactant)
+print 'eliminating repeated reactions ...'
+os.system('python reduce-species.py '+reactant) 
+#print 'writing reactions in catmap format ...'
+#os.system('python catmap.py '+reactant)
 #print '='*20, 'removing C-C steps with TS > 1.5 eV'
 #os.system('post-process-C-C-steps-v6.py '+reactant+' > catmap-rxns'+reactant)
 #print 'did catmap remove --O assisted dehydrogenation'
