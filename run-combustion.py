1c09f7fe348a4b0a1fd5a82816c66ba6e21244f6
aljamaha <aljamaha@stanford.edu>
2019-10-11 13:17:35 -0700

    removed scripts outsdie catmpap/graphviz

--- run-combustion.py
+++ run-combustion.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 reactant = sys.argv[1]	#name of the hydrocarbon reactant
 
 print 'generating reactions list ...'
@@ -10,9 +9,6 @@ print 'eliminating repeated reactions ...'
 os.system('python reduce-species.py '+reactant) 
 #print 'writing reactions in catmap format ...'
 #os.system('python catmap.py '+reactant)
-#print '='*20, 'removing C-C steps with TS > 1.5 eV'
-#os.system('post-process-C-C-steps-v6.py '+reactant+' > catmap-rxns'+reactant)
-#print 'did catmap remove --O assisted dehydrogenation'
 #print '='*20, 'graphviz'
 #os.system('python graphviz-17.py '+reactant)
 
