6ca06b99271fe021057448e154a735f60818fd41
aljamaha <aljamaha@stanford.edu>
2019-10-14 09:51:47 -0700

    removing repeated catmap rxns

--- run-combustion.py
+++ run-combustion.py
@@ -7,8 +7,8 @@ print 'generating reactions list ...'
 os.system('python generate-species.py '+reactant)
 print 'eliminating repeated reactions ...'
 os.system('python reduce-species.py '+reactant) 
-#print 'writing reactions in catmap format ...'
-#os.system('python catmap.py '+reactant)
+print 'writing reactions in catmap format ...'
+os.system('python catmap.py '+reactant)
 #print '='*20, 'graphviz'
 #os.system('python graphviz-17.py '+reactant)
 
